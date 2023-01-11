5afaaefdf54ed3d5ac6456948cc258643085b4d2
Fernando Diogo <aluno22111@ipt.pt>
2023-01-11 23:40:08 +0000

    Adição Kotlin Class para o Login e Registo. Alterações no Layout. Extração de estilos

--- Restaurants4you/app/src/main/java/com/dam/restaurants4you/MainActivity.kt
+++ Restaurants4you/app/src/main/java/com/dam/restaurants4you/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.dam.restaurants4you
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val btnLogin = findViewById<View>(R.id.btnLogin) as Button
+        val btnRegisto = findViewById<View>(R.id.btnRegisto) as Button
+
+        btnLogin.setOnClickListener(View.OnClickListener {
+            val it = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(it)
+        })
+
+        btnRegisto.setOnClickListener(View.OnClickListener {
+            val it = Intent(this@MainActivity, RegistoActivity::class.java)
+            startActivity(it)
+        })
+
     }
 }
---
+++ Restaurants4you/app/src/main/java/com/dam/restaurants4you/RegistoActivity.kt
@@ -0,0 +1,12 @@
+package com.dam.restaurants4you
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registo)
+    }
+}
