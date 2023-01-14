572a46cf9cf848ed71d75b8be62f4f922702f0e9
Fernando Diogo <aluno22111@ipt.pt>
2023-01-14 16:54:06 +0000

    Adição de estilo ao login

--- Restaurants4you/app/src/main/java/com/dam/restaurants4you/activity/MainActivity.kt
+++ Restaurants4you/app/src/main/java/com/dam/restaurants4you/activity/MainActivity.kt
@@ -12,33 +12,10 @@ import com.dam.restaurants4you.fragmentos.Fragmentos
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
 
 
-        val btnLogin = findViewById<View>(R.id.btnLogin) as Button
-        val btnRegisto = findViewById<View>(R.id.btnRegisto) as Button
-        val btnmapa = findViewById<View>(R.id.mapaa) as Button
-        val btnfrags = findViewById<View>(R.id.frags) as Button
-
-        btnLogin.setOnClickListener(View.OnClickListener {
-            val it = Intent(this@MainActivity, LoginActivity::class.java)
-            startActivity(it)
-        })
-
-        btnRegisto.setOnClickListener(View.OnClickListener {
-            val it = Intent(this@MainActivity, RegistoActivity::class.java)
-            startActivity(it)
-        })
-
-        btnmapa.setOnClickListener(View.OnClickListener {
-            val it = Intent(this@MainActivity, MapaActivity::class.java)
-            startActivity(it)
-        })
-
-        btnfrags.setOnClickListener(View.OnClickListener {
-            val it = Intent(this@MainActivity, Fragmentos::class.java)
-            startActivity(it)
-        })
+        
 
     }
 }
